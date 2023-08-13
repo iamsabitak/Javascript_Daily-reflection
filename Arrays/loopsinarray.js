80fc2f875bb13b0e7bf0d0385b87339fabc91303
sabita <sabitakhadka091@gmail.com>
2023-08-13 14:36:40 +0545

    updating

---
+++ Arrays/loopsinarray.js
@@ -0,0 +1,22 @@
+let num = [3, 5, 1, 2, 4]
+for(let i=0; i<num. length; i++){
+    console. log (num[i])}
+
+    // ForEach Loop
+    num.forEach((element) => {
+        console.log(element * element)
+    })
+
+    // Array.from
+let name = "Sabita "
+let arr = Array. from( name )
+console. log(arr)
+
+// for...of
+ for (let i of name){
+    console. log(i)
+}
+// for...in
+for (let i in name){
+    console. log(name[i])
+}
