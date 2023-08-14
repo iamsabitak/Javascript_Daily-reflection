43bd0370ec7d1f66e76c9999b1ae69b24f25f482
sabita <sabitakhadka091@gmail.com>
2023-08-14 20:16:53 +0545

    new file added

---
+++ Arrays/ps.js
@@ -0,0 +1,37 @@
+//Chapter 5 practice set
+//Problem no 1
+let arr =[1,2,3,4,5]
+let a = 6
+arr.push(a)
+console.log(arr)
+
+//Problem no 2
+let arr1 = [1,2,3,4,5,6,]
+let b = 0;
+do {
+    arr.push(a)
+} while(b!=0)
+console.log(arr)
+
+//Problem no 3
+let arr2 = [4,8,12,982,3,34,]
+let a1 = (x) => {
+    return x%2 ==0
+}
+let n = arr2.filter(a1)
+console.log(n)
+
+//Problem no 4
+let arr3 = [4,8,12,982,3,34,]
+let a2 = (x) => {
+    return x*x
+}
+let s = arr3.map(a2)
+console.log(s)
+
+//Problem no 5
+let arr4 = [1,2,3, 4]
+let n1 = arr4. reduce( (x1, x2) => {
+return x1 * x2
+})
+ console. log (n1)
