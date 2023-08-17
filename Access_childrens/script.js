3eb4cc1764b383146abd3ae469996e3810caf55e
sabita <sabitakhadka091@gmail.com>
2023-08-17 15:45:59 +0545

    updating

---
+++ Access_childrens/script.js
@@ -0,0 +1,9 @@
+console.log(document.body.firstChild)
+console.log(document.body.lastChild)
+console.log(document.body.childNodes)
+let array = Array.from(document.body.childNodes) 
+console.log(array)
+console. log(document.body.firstChild)
+a = document. body. firstChild
+console. log(a.parentNode)
+console. log(a.parentElement)
