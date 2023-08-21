cbb733be14faf0a07a77b1f2765ad5c47dbbc78a
sabita <sabitakhadka091@gmail.com>
2023-08-21 14:31:11 +0545

    updating

---
+++ Insertion_Methods/index.js
@@ -0,0 +1,10 @@
+let a = document.getElementsByTagName("div")[0];
+// a.innerHTML = a.innerHTML + "<h1>Hello world</h1>";
+
+let div = document. createElement ( 'div');
+div.innerHTML ='<h1>Hello World!</h1>';
+a.appendChild(div);
+// a.prepend (div);
+// a.before(div);
+//  a.after(div);
+//a.replaceWith('div');
---
+++ innerHTML_outerHTML/script.js
@@ -0,0 +1,13 @@
+console.log(document.getElementsByTagName("span")[0]);
+console.dir(document.getElementsByTagName("span")[0]);
+console.log(document.getElementsByClassName("red")[0]);
+console.log(document.body.firstChild)
+console.log(document.body.firstElementChild)
+console.log(first.innerHTML)
+console.log(first.innerHTML="<i> iam sabita </i>")
+console.log(first.outerHTML)
+console.log(first.innerHTML="<div> i am div </div>")
+// document.body.firstChild
+// document.body.firstChild.data
+// document.body.firstChild.nodeValue
+console.log(document.body.textContent)
