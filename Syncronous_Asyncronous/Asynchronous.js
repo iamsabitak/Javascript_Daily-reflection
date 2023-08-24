3a635fd7dda0075883bcaf94666582f276d5a54f
sabita <sabitakhadka091@gmail.com>
2023-08-24 16:00:41 +0545

    new file implemented

---
+++ Syncronous_Asyncronous/Asynchronous.js
@@ -0,0 +1,6 @@
+// Asynchronous programming
+console.log("Start")
+setTimeout(function () {
+    console.log("Hey This is me Sabita")
+},2000)
+console.log("End")
---
+++ Syncronous_Asyncronous/Syncronous_programming.js
@@ -0,0 +1,5 @@
+//Syncronous programming
+let a = prompt("What is your name?")
+let b = prompt("What is your age?")
+let c = prompt("What is your job?")
+console.log("My name is" + a + "and My age is " + b + " or I am still a student" + c)
