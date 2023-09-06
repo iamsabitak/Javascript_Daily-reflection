d7b1bb13399b3625e009351f495c22c4f8c878f7
sabita <sabitakhadka091@gmail.com>
2023-09-06 17:05:24 +0545

    updating

--- Rock Paper Scissor/script.js
+++ Rock Paper Scissor/script.js
@@ -1,27 +1,28 @@
-let user = prompt("Rock Paper Scissor");
-let comp = Math.floor(Math.random(0, 1, 2));
+// let choice = ["Rock", "Paper", "Scissor"];
+function playGame() {
+let user = prompt("Enter the number");
+let comp = Math.floor(Math.random() * 3);
 // 0 for scissor
 // 1 for rock
 // 2 for paper
-if (comp > 2) {
-  console.log("Computern - Invalid Number");
+if (comp >2) {
+  console.timeLog("Computern - Invalid Number");
 } else if (user > 2) {
   console.log("User - Invalid Number");
-}
-if (comp == 0 && user == 1) {
+} else if (comp == 0 && user == 1) {
   console.log("Computer - Scissor");
   console.log("User - Rock");
-}
-if (comp == 1 && user == 2) {
+} else if (comp == 1 && user == 2) {
   console.log("Computer - Rock");
   console.log("User - Paper");
-}
-if (comp == 2 && user == 0) {
+} else if (comp == 2 && user == 0) {
   console.log("Computer - Paper");
   console.log("User - Scissor");
 }
 
-if (
+if (comp == user) {
+  console.log("draw");
+} else if (
   (comp == 0 && user == 1) ||
   (comp == 1 && user == 2) ||
   (comp == 2 && user == 0)
@@ -30,3 +31,5 @@ if (
 } else {
   console.log("Computer Win..");
 }
+}
+const result = playGame();
